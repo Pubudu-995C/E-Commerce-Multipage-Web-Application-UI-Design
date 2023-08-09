929a211a22e2c6d64bbd80d549624a37398df3f0
Pubudu-995C <pubudu.chamara753@gmail.com>
2023-08-10 00:00:58 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,26 @@
+const bar = document.getElementById("bar");
+const close = document.getElementById("close");
+const nav = document.getElementById("navbar");
+
+if (bar) {
+  bar.addEventListener("click", () => {
+    nav.classList.add("active");
+  });
+}
+if (close) {
+  close.addEventListener("click", () => {
+    nav.classList.remove("active");
+  });
+}
+
+
+// =================Product Image Slider=================
+
+const MainImg = document.getElementById("MainImg");
+const smallimg = document.getElementsByClassName("small-img");
+
+for (let i = 0; i < smallimg.length; i++) {
+  smallimg[i].onclick = function () {
+    MainImg.src = smallimg[i].src;
+  };
+}
